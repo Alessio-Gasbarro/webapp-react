a17df6cdfcf881e6e5f2e5f3a2d784eb8570c399
Alessio-Gasbarro <gasbarro.developer@gmail.com>
2025-05-31 12:45:04 +0200

    [UPD] Restructured + Add Header

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,17 @@
 import DefaultLayout from "../layouts/DefaultLayouts"
-import { BrowserRouter, Routes, Route } from "react-router"
+import { BrowserRouter, Routes, Route } from "react-router-dom"
 import HomePage from "../pages/HomePage"
 import MoviePage from "../pages/MoviePage"
+import NotFoundPage from "../pages/NotFoundPage"
 
 function App() {
-
   return (
     <BrowserRouter>
       <Routes>
         <Route element={<DefaultLayout />}>
           <Route index element={<HomePage />}></Route>
           <Route path="/movies/:id" element={<MoviePage />}></Route>
+          <Route path="*" element={<NotFoundPage />}></Route>
         </Route>
       </Routes>
     </BrowserRouter>
